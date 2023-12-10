9768409eacbb631aabfab564687dc79f8892af45
Matheus Jessé <matheus_jesse@hotmail.com>
2023-12-09 23:27:30 -0300

    feat: exercicio um utilizando where

--- Program.cs
+++ Program.cs
@@ -1,2 +1,6 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿int[] numeros = { 1, 2, 3, 4, 5, 6, 7, 8, 9, 10 };
+
+foreach(var item in numeros.Where(x => x > 4))
+{
+    Console.WriteLine(item);
+}
