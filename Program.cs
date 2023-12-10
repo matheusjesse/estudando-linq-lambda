dbd2cc36c27d44bcc9f46a75b0553a2fcc746c84
Matheus Jessé <matheus_jesse@hotmail.com>
2023-12-09 23:43:30 -0300

    feat: aplicando linq no array de string

--- Program.cs
+++ Program.cs
@@ -1,6 +1,16 @@
-﻿int[] numeros = { 8, 5, 9, 10, 2, 3, 7, 6 };
+﻿// int[] numeros = { 8, 5, 9, 10, 2, 3, 7, 6 };
 
-foreach(var item in numeros.Where(x => x > 4).OrderBy(n => n))
+// foreach(var item in numeros.Where(x => x > 4).OrderBy(n => n))
+// {
+//     Console.WriteLine(item);
+// }
+
+string[] cores = {"preto", "branco", "vermelho", "azul" };
+
+var resultado = cores.Where(x => x.Contains("e"));
+
+foreach(string cor in resultado)
 {
-    Console.WriteLine(item);
+    Console.WriteLine(cor);
 }
+ 
