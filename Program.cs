f678bb0c241229432f914330155d0d24ce9ee8e0
Matheus Jessé <matheus_jesse@hotmail.com>
2023-12-09 23:35:50 -0300

    feat: implementando orderby para ordernar os item

--- Program.cs
+++ Program.cs
@@ -1,6 +1,6 @@
-﻿int[] numeros = { 1, 2, 3, 4, 5, 6, 7, 8, 9, 10 };
+﻿int[] numeros = { 8, 5, 9, 10, 2, 3, 7, 6 };
 
-foreach(var item in numeros.Where(x => x > 4))
+foreach(var item in numeros.Where(x => x > 4).OrderBy(n => n))
 {
     Console.WriteLine(item);
 }
